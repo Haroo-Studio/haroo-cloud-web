3eacdc6307772f8c64286f92bdf27c659ada4552
soomtong <soomtong@gmail.com>
2014-11-29 15:43:11 +0900

    `update` selector display

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,16 +1,26 @@
 $('document').ready(function () {
+    // responsive controls
     $('.nav-menu-button').on('click', function (e) {
         $('#nav').toggleClass('active');
     });
-    viewDocument();
+
+    // document view control
+    var $list = $('.haroonote-item');
+    var $content = $('.haroonote-content');
+
+    viewDocument($list, $content);
     $('#list').on('click', '.haroonote-item', function (e) {
-        var id = $(this).data('id');
-        viewDocument(id);
+        var bindID = $(this).data('id');
+        viewDocument($list, $content, bindID);
     })
 });
 
-function viewDocument(id) {
+function viewDocument(list, content, id) {
     if (!id) id = 1;
-    $('.haroonote-content').hide();
-    $('.haroonote-content[data-id=' + id + ']').show();
+    var selectClass = 'haroonote-item-selected';
+
+    list.removeClass(selectClass);
+    content.hide();
+    list.closest('[data-id=' + id + ']').addClass(selectClass);
+    content.closest('[data-id=' + id + ']').show();
 }
