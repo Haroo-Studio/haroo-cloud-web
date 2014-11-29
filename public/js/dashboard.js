ccc6a244b00139fa941e58fcc306e1e16e7a003e
soomtong <soomtong@gmail.com>
2014-11-29 15:32:53 +0900

    `update` change view docs

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -2,4 +2,15 @@ $('document').ready(function () {
     $('.nav-menu-button').on('click', function (e) {
         $('#nav').toggleClass('active');
     });
+    viewDocument();
+    $('#list').on('click', '.haroonote-item', function (e) {
+        var id = $(this).data('id');
+        viewDocument(id);
+    })
 });
+
+function viewDocument(id) {
+    if (!id) id = 1;
+    $('.haroonote-content').hide();
+    $('.haroonote-content[data-id=' + id + ']').show();
+}
