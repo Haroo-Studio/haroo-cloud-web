99cc0945fc711a83b2b467dc7fe3be839f78fb26
soomtong <soomtong@gmail.com>
2014-12-12 00:03:58 +0900

    `update` test graph lib, not exactly works for date type

--- route/stat.js
+++ route/stat.js
@@ -8,272 +8,321 @@ exports.system = function (req, res) {
 
 exports.documentStat = function (req, res) {
     var params = {
-        data: [
-            {
-                "year": "1945",
-                "sightings": 6
-            },
-            {
-                "year": "1946",
-                "sightings": 8
-            },
-            {
-                "year": "1947",
-                "sightings": 34
-            },
-            {
-                "year": "1948",
-                "sightings": 8
-            },
-            {
-                "year": "1949",
-                "sightings": 15
-            },
-            {
-                "year": "1950",
-                "sightings": 25
-            },
-            {
-                "year": "1951",
-                "sightings": 20
-            },
-            {
-                "year": "1952",
-                "sightings": 48
-            },
-            {
-                "year": "1953",
-                "sightings": 34
-            },
-            {
-                "year": "1954",
-                "sightings": 50
-            },
-            {
-                "year": "1955",
-                "sightings": 31
-            },
-            {
-                "year": "1956",
-                "sightings": 38
-            },
-            {
-                "year": "1957",
-                "sightings": 67
-            },
-            {
-                "year": "1958",
-                "sightings": 40
-            },
-            {
-                "year": "1959",
-                "sightings": 47
-            },
-            {
-                "year": "1960",
-                "sightings": 64
-            },
-            {
-                "year": "1961",
-                "sightings": 39
-            },
-            {
-                "year": "1962",
-                "sightings": 55
-            },
-            {
-                "year": "1963",
-                "sightings": 75
-            },
-            {
-                "year": "1964",
-                "sightings": 77
-            },
-            {
-                "year": "1965",
-                "sightings": 167
-            },
-            {
-                "year": "1966",
-                "sightings": 169
-            },
-            {
-                "year": "1967",
-                "sightings": 178
-            },
-            {
-                "year": "1968",
-                "sightings": 183
-            },
-            {
-                "year": "1969",
-                "sightings": 138
-            },
-            {
-                "year": "1970",
-                "sightings": 126
-            },
-            {
-                "year": "1971",
-                "sightings": 110
-            },
-            {
-                "year": "1972",
-                "sightings": 146
-            },
-            {
-                "year": "1973",
-                "sightings": 209
-            },
-            {
-                "year": "1974",
-                "sightings": 241
-            },
-            {
-                "year": "1975",
-                "sightings": 279
-            },
-            {
-                "year": "1976",
-                "sightings": 246
-            },
-            {
-                "year": "1977",
-                "sightings": 239
-            },
-            {
-                "year": "1978",
-                "sightings": 301
-            },
-            {
-                "year": "1979",
-                "sightings": 221
-            },
-            {
-                "year": "1980",
-                "sightings": 211
-            },
-            {
-                "year": "1981",
-                "sightings": 146
-            },
-            {
-                "year": "1982",
-                "sightings": 182
-            },
-            {
-                "year": "1983",
-                "sightings": 132
-            },
-            {
-                "year": "1984",
-                "sightings": 172
-            },
-            {
-                "year": "1985",
-                "sightings": 192
-            },
-            {
-                "year": "1986",
-                "sightings": 173
-            },
-            {
-                "year": "1987",
-                "sightings": 193
-            },
-            {
-                "year": "1988",
-                "sightings": 203
-            },
-            {
-                "year": "1989",
-                "sightings": 220
-            },
-            {
-                "year": "1990",
-                "sightings": 217
-            },
-            {
-                "year": "1991",
-                "sightings": 210
-            },
-            {
-                "year": "1992",
-                "sightings": 228
-            },
-            {
-                "year": "1993",
-                "sightings": 285
-            },
-            {
-                "year": "1994",
-                "sightings": 381
-            },
-            {
-                "year": "1995",
-                "sightings": 1336
-            },
-            {
-                "year": "1996",
-                "sightings": 862
-            },
-            {
-                "year": "1997",
-                "sightings": 1248
-            },
-            {
-                "year": "1998",
-                "sightings": 1812
-            },
-            {
-                "year": "1999",
-                "sightings": 2906
-            },
-            {
-                "year": "2000",
-                "sightings": 2780
-            },
-            {
-                "year": "2001",
-                "sightings": 3105
-            },
-            {
-                "year": "2002",
-                "sightings": 3176
-            },
-            {
-                "year": "2003",
-                "sightings": 3896
-            },
-            {
-                "year": "2004",
-                "sightings": 4208
-            },
-            {
-                "year": "2005",
-                "sightings": 3996
-            },
-            {
-                "year": "2006",
-                "sightings": 3590
-            },
-            {
-                "year": "2007",
-                "sightings": 4195
-            },
-            {
-                "year": "2008",
-                "sightings": 4705
-            },
-            {
-                "year": "2009",
-                "sightings": 4297
-            },
-            {
-                "year": "2010",
-                "sightings": 2531
-            }
-        ]
+        documents: [{
+            "date": "1",
+            "sightings": 6
+        }, {
+            "date": "2",
+            "sightings": 8
+        }, {
+            "date": "3",
+            "sightings": 34
+        }, {
+            "date": "4",
+            "sightings": 8
+        }, {
+            "date": "5",
+            "sightings": 15
+        }, {
+            "date": "6",
+            "sightings": 25
+        }, {
+            "date": "7",
+            "sightings": 20
+        }, {
+            "date": "8",
+            "sightings": 48
+        }, {
+            "date": "9",
+            "sightings": 34
+        }, {
+            "date": "10",
+            "sightings": 50
+        }, {
+            "date": "11",
+            "sightings": 31
+        }, {
+            "date": "12",
+            "sightings": 38
+        }, {
+            "date": "13",
+            "sightings": 67
+        }, {
+            "date": "14",
+            "sightings": 40
+        }, {
+            "date": "15",
+            "sightings": 47
+        }, {
+            "date": "16",
+            "sightings": 64
+        }, {
+            "date": "17",
+            "sightings": 39
+        }, {
+            "date": "18",
+            "sightings": 55
+        }, {
+            "date": "19",
+            "sightings": 75
+        }, {
+            "date": "20",
+            "sightings": 77
+        }, {
+            "date": "21",
+            "sightings": 167
+        }, {
+            "date": "22",
+            "sightings": 169
+        }, {
+            "date": "23",
+            "sightings": 178
+        }, {
+            "date": "24",
+            "sightings": 183
+        }, {
+            "date": "25",
+            "sightings": 138
+        }, {
+            "date": "26",
+            "sightings": 126
+        }, {
+            "date": "27",
+            "sightings": 110
+        }, {
+            "date": "28",
+            "sightings": 146
+        }, {
+            "date": "29",
+            "sightings": 209
+        }, {
+            "date": "30",
+            "sightings": 241
+        }, {
+            "date": "31",
+            "sightings": 279
+        }, {
+            "date": "32",
+            "sightings": 246
+        }, {
+            "date": "33",
+            "sightings": 239
+        }, {
+            "date": "34",
+            "sightings": 301
+        }, {
+            "date": "35",
+            "sightings": 221
+        }, {
+            "date": "36",
+            "sightings": 211
+        }, {
+            "date": "37",
+            "sightings": 146
+        }, {
+            "date": "38",
+            "sightings": 182
+        }, {
+            "date": "39",
+            "sightings": 132
+        }, {
+            "date": "40",
+            "sightings": 172
+        }, {
+            "date": "41",
+            "sightings": 192
+        }, {
+            "date": "42",
+            "sightings": 173
+        }, {
+            "date": "43",
+            "sightings": 193
+        }, {
+            "date": "44",
+            "sightings": 203
+        }, {
+            "date": "45",
+            "sightings": 220
+        }, {
+            "date": "46",
+            "sightings": 217
+        }, {
+            "date": "47",
+            "sightings": 210
+        }, {
+            "date": "48",
+            "sightings": 228
+        }, {
+            "date": "49",
+            "sightings": 285
+        }, {
+            "date": "50",
+            "sightings": 381
+        }, {
+            "date": "51",
+            "sightings": 1336
+        }, {
+            "date": "52",
+            "sightings": 862
+        }, {
+            "date": "53",
+            "sightings": 1248
+        }, {
+            "date": "54",
+            "sightings": 1812
+        }, {
+            "date": "55",
+            "sightings": 2906
+        }, {
+            "date": "56",
+            "sightings": 2780
+        }, {
+            "date": "57",
+            "sightings": 3105
+        }, {
+            "date": "58",
+            "sightings": 3176
+        }, {
+            "date": "59",
+            "sightings": 3896
+        }, {
+            "date": "60",
+            "sightings": 4208
+        }, {
+            "date": "61",
+            "sightings": 3996
+        }, {
+            "date": "62",
+            "sightings": 3590
+        }, {
+            "date": "63",
+            "sightings": 4195
+        }, {
+            "date": "64",
+            "sightings": 4705
+        }, {
+            "date": "65",
+            "sightings": 4297
+        }, {
+            "date": "66",
+            "sightings": 4531
+        }],
+        public_documents_status: [{
+            "date": "12",
+            "count": 228
+        }, {
+            "date": "13",
+            "count": 285
+        }, {
+            "date": "14",
+            "count": 381
+        }, {
+            "date": "15",
+            "count": 1336
+        }, {
+            "date": "16",
+            "count": 862
+        }, {
+            "date": "17",
+            "count": 1248
+        }, {
+            "date": "18",
+            "count": 1812
+        }, {
+            "date": "19",
+            "count": 2906
+        }, {
+            "date": "20",
+            "count": 2780
+        }, {
+            "date": "22",
+            "count": 3105
+        }, {
+            "date": "23",
+            "count": 3176
+        }, {
+            "date": "24",
+            "count": 3896
+        }, {
+            "date": "25",
+            "count": 4208
+        }, {
+            "date": "26",
+            "count": 3996
+        }, {
+            "date": "27",
+            "count": 3590
+        }, {
+            "date": "28",
+            "count": 4195
+        }, {
+            "date": "29",
+            "count": 4705
+        }, {
+            "date": "30",
+            "count": 4297
+        }, {
+            "date": "31",
+            "count": 2531
+        }],
+        attached_file_size: [{
+            "date": "2",
+            "count": "1228"
+        }, {
+            "date": "3",
+            "count": "1285"
+        }, {
+            "date": "4",
+            "count": "1381"
+        }, {
+            "date": "5",
+            "count": "1336"
+        }, {
+            "date": "6",
+            "count": "1862"
+        }, {
+            "date": "7",
+            "count": "1248"
+        }, {
+            "date": "8",
+            "count": "1812"
+        }, {
+            "date": "9",
+            "count": "2906"
+        }, {
+            "date": "10",
+            "count": "2780"
+        }, {
+            "date": "11",
+            "count": "3105"
+        }, {
+            "date": "12",
+            "count": "3176"
+        }, {
+            "date": "13",
+            "count": "3896"
+        }, {
+            "date": "14",
+            "count": "4208"
+        }, {
+            "date": "15",
+            "count": "3996"
+        }, {
+            "date": "16",
+            "count": "3590"
+        }, {
+            "date": "17",
+            "count": "4195"
+        }, {
+            "date": "18",
+            "count": "4705"
+        }, {
+            "date": "19",
+            "count": "4297"
+        }, {
+            "date": "20",
+            "count": "2531"
+        }]
     };
 
     res.json(params);
